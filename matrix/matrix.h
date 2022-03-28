874918da01c2c3b8c81edad16651650e7bb6ad96
Ksenija Okuneva <okuneva.k@phystech.edu>
2022-03-28 19:26:57 +0300

    update

--- matrix/matrix.h
+++ matrix/matrix.h
@@ -54,7 +54,6 @@ class Matrix {
     }
     return *this;
   }
-
   Matrix<T, Rows, Cols> &operator-=(const Matrix<T, Rows, Cols> &other) {
     for (size_t i = 0; i < Rows; ++i) {
       for (size_t j = 0; j < Cols; ++j) {
@@ -64,21 +63,7 @@ class Matrix {
     return *this;
   }
 
-  Matrix<T, Rows, Cols> &operator*=(const Matrix<T, Cols, Cols> &other) {
-    Matrix<T, Rows, Cols> new_matrix;
-    for (size_t i = 0; i < Rows; ++i) {
-      for (size_t j = 0; j < Cols; ++j) {
-        new_matrix.curr[i][j] = 0;
-        for (size_t k = 0; k < Cols; ++k) {
-          new_matrix.curr[i][j] += curr[i][k] * other.curr[k][j];
-        }
-      }
-    }
-    *this = new_matrix;
-    return *this;
-  }
-
-  Matrix<T, Rows, Cols> &operator*=(const int64_t num) {
+  Matrix<T, Rows, Cols> &operator*=(const int64_t &num) {
     for (size_t i = 0; i < Rows; i++) {
       for (size_t j = 0; j < Cols; j++) {
         curr[i][j] *= num;
@@ -86,16 +71,6 @@ class Matrix {
     }
     return *this;
   }
-  Matrix<T, Rows, Cols> operator*(const int64_t num) {
-    Matrix<T, Rows, Cols> new_matrix;
-    for (size_t i = 0; i < Rows; i++) {
-      for (size_t j = 0; j < Cols; j++) {
-        new_matrix.curr[i][j] = curr[i][j] * num;
-      }
-    }
-    return new_matrix;
-  }
-
   Matrix<T, Rows, Cols> &operator/=(const int64_t &num) {
     for (size_t i = 0; i < Rows; i++) {
       for (size_t j = 0; j < Cols; j++) {
@@ -104,37 +79,39 @@ class Matrix {
     }
     return *this;
   }
-  Matrix<T, Rows, Cols> operator/(const int64_t num) {
-    Matrix<T, Rows, Cols> new_matrix;
-    for (size_t i = 0; i < Rows; i++) {
-      for (size_t j = 0; j < Cols; j++) {
-        new_matrix.curr[i][j] = curr[i][j] / num;
-      }
-    }
-    return new_matrix;
-  }
 
-  bool operator==(const Matrix &other) const {
-    for (size_t i = 0; i < Rows; i++) {
-      for (size_t j = 0; j < Cols; j++) {
-        if (curr[i][j] != other.curr[i][j]) {
-          return false;
+  Matrix<T, Rows, Cols> &operator*=(const Matrix<T, Cols, Cols> &other) {
+    T line[Cols];
+    for (size_t i = 0; i < Rows; ++i) {
+      for (size_t j = 0; j < Cols; ++j) {
+        line[j] = 0;
+        for (size_t k = 0; k < Cols; ++k) {
+          line[j] += curr[i][k] * other.curr[k][j];
         }
       }
-    }
-    return true;
-  }
-  bool operator!=(const Matrix &other) const {
-    for (size_t i = 0; i < Rows; i++) {
-      for (size_t j = 0; j < Cols; j++) {
-        if (curr[i][j] != other.curr[i][j]) {
-          return true;
-        }
+      for (size_t j = 0; j < Cols; ++j) {
+        curr[i][j] = line[j];
       }
     }
-    return false;
+    return *this;
   }
 };
+
+template <class T, size_t Rows, size_t Cols>
+bool operator==(const Matrix<T, Cols, Rows> &matrix, const Matrix<T, Cols, Rows> &other) {
+  for (size_t i = 0; i < Rows; i++) {
+    for (size_t j = 0; j < Cols; j++) {
+      if (matrix.curr[i][j] != other.curr[i][j]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+template <class T, size_t Rows, size_t Cols>
+bool operator!=(const Matrix<T, Cols, Rows> &matrix, const Matrix<T, Cols, Rows> &other) {
+  return !(matrix == other);
+}
 
 template <class T, size_t Rows, size_t Cols>
 Matrix<T, Cols, Rows> GetTransposed(Matrix<T, Rows, Cols> &matrix) {
@@ -147,11 +124,31 @@ Matrix<T, Cols, Rows> GetTransposed(Matrix<T, Rows, Cols> &matrix) {
   return new_matrix;
 }
 
+template <class T, size_t Rows, size_t Cols>
+Matrix<T, Rows, Cols> operator*(const int64_t num, const Matrix<T, Rows, Cols> matrix) {
+  Matrix<T, Rows, Cols> new_matrix = matrix;
+  new_matrix *= num;
+  return new_matrix;
+}
+template <class T, size_t Rows, size_t Cols>
+Matrix<T, Rows, Cols> operator*(const Matrix<T, Rows, Cols> matrix, const int64_t num) {
+  Matrix<T, Rows, Cols> new_matrix = matrix;
+  new_matrix *= num;
+  return new_matrix;
+}
+template <class T, size_t Rows, size_t Cols>
+Matrix<T, Rows, Cols> operator/(const Matrix<T, Rows, Cols> matrix, const int64_t num) {
+  Matrix<T, Rows, Cols> new_matrix = matrix;
+  new_matrix /= num;
+  return new_matrix;
+}
+
 template <class T, size_t N, size_t M, size_t P>
 Matrix<T, N, P> operator*(const Matrix<T, N, M> &a, const Matrix<T, M, P> &b) {
   Matrix<T, N, P> new_matrix;
   for (size_t i = 0; i < N; ++i) {
     for (size_t j = 0; j < P; ++j) {
+      new_matrix.curr[i][j] = 0;
       for (size_t k = 0; k < M; ++k) {
         new_matrix.curr[i][j] += a.curr[i][k] * b.curr[k][j];
       }
@@ -161,46 +158,15 @@ Matrix<T, N, P> operator*(const Matrix<T, N, M> &a, const Matrix<T, M, P> &b) {
 }
 
 template <class T, size_t Rows, size_t Cols>
-Matrix<T, Rows, Cols> operator*(const int64_t num, const Matrix<T, Rows, Cols> matrix) {
-  Matrix<T, Rows, Cols> new_matrix;
-  for (size_t i = 0; i < Rows; i++) {
-    for (size_t j = 0; j < Cols; j++) {
-      new_matrix.curr[i][j] = matrix(i, j) * num;
-    }
-  }
-  return new_matrix;
-}
-
-template <class T, size_t Rows, size_t Cols>
-Matrix<T, Rows, Cols> operator/(const int64_t num, const Matrix<T, Rows, Cols> matrix) {
-  Matrix<T, Rows, Cols> new_matrix;
-  for (size_t i = 0; i < Rows; i++) {
-    for (size_t j = 0; j < Cols; j++) {
-      new_matrix.curr[i][j] = matrix(i, j) / num;
-    }
-  }
-  return new_matrix;
-}
-
-template <class T, size_t Rows, size_t Cols>
 Matrix<T, Rows, Cols> operator+(const Matrix<T, Rows, Cols> &matrix, const Matrix<T, Rows, Cols> &other) {
-  Matrix<T, Rows, Cols> new_matrix;
-  for (size_t i = 0; i < Rows; ++i) {
-    for (size_t j = 0; j < Cols; ++j) {
-      new_matrix.curr[i][j] = matrix.curr[i][j] + other.curr[i][j];
-    }
-  }
+  Matrix<T, Rows, Cols> new_matrix = matrix;
+  new_matrix += other;
   return new_matrix;
 }
-
 template <class T, size_t Rows, size_t Cols>
 Matrix<T, Rows, Cols> operator-(const Matrix<T, Rows, Cols> &matrix, const Matrix<T, Rows, Cols> &other) {
-  Matrix<T, Rows, Cols> new_matrix;
-  for (size_t i = 0; i < Rows; ++i) {
-    for (size_t j = 0; j < Cols; ++j) {
-      new_matrix.curr[i][j] = matrix.curr[i][j] - other.curr[i][j];
-    }
-  }
+  Matrix<T, Rows, Cols> new_matrix = matrix;
+  new_matrix -= other;
   return new_matrix;
 }
 
@@ -213,7 +179,6 @@ std::istream &operator>>(std::istream &is, Matrix<T, Rows, Cols> &matrix) {
   }
   return is;
 }
-
 template <class T, size_t Rows, size_t Cols>
 std::ostream &operator<<(std::ostream &os, const Matrix<T, Rows, Cols> &matrix) {
   for (size_t i = 0; i < Rows; ++i) {
